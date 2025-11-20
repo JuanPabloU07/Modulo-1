cd1a5cb1fa5876feb215530e9be1c1d3ddb7ff97
Juan Pablo <juanpablou07@gmail.com>
2025-11-20 06:53:12 -0500

    ajuste de código

--- semana2/taller_sem2.py
+++ semana2/taller_sem2.py
@@ -7,7 +7,7 @@ all_quantity = []
 #Inicio del ciclo y mostrar el menú principal
 while True:
     print("")
-    print("\033[95m-- MENÚ INVENTARIO --\033[0m")
+    print(" MENÚ INVENTARIO ")
     print("1. Agregar producto")
     print("2. Mostrar inventario")
     print("3. Calcular estadísticas")
@@ -19,7 +19,7 @@ while True:
         user_election = int(input("Escriba el número de la opción seleccionada: "))
         print("")
         if user_election <= 0 or user_election > 4:
-            print("\033[91m¡ERROR!. Número no contemplado en la lista.\033[0m")
+            print("¡ERROR!. Número no contemplado en la lista.")
 
 #Sección para agregar producto y la información (nombre, precio y cantidad)
         elif user_election == 1:
@@ -27,31 +27,31 @@ while True:
                 try:
                     quantity = int(input("¿Cuántos productos desea ingresar?: "))
                     if quantity <= 0:
-                        print("\033[91m¡ERROR!. Ingrese un número positivo y diferente a 0.\033[0m")
+                        print("¡ERROR!. Ingrese un número positivo y diferente a 0.")
                         continue
                     else:
                         for i in range(quantity):
                             product = {}
                             print("")
-                            print(f"\033[96mProducto #{i+1}\033[0m")
+                            print(f"Producto #{i+1}")
                             name_product = input("Nombre: ")
                             product["nombre"] = name_product
                             while True:
                                 try:
                                     price_product = float(input("Precio: "))
                                     if price_product <= 0:
-                                        print("\033[91m¡ERROR!. El precio debe ser mayor a $0.\033[0m")
+                                        print("¡ERROR!. El precio debe ser mayor a $0.")
                                         continue
                                     else:
                                         product["precio"] = price_product
                                     break
                                 except ValueError:
-                                    print("\033[91m¡ERROR!. Debe ingresar un número.\033[0m")
+                                    print("[91m¡ERROR!. Debe ingresar un número.")
                             while True:
                                 try:
                                     quantity_product = int(input("Cantidad: "))
                                     if quantity_product <= 0:
-                                        print("\033[91m¡ERROR!. La cantidad debe ser mayor a 0.\033[0m")
+                                        print("¡ERROR!. La cantidad debe ser mayor a 0.")
                                         continue
                                     else:
                                         total_price = price_product * quantity_product
@@ -60,31 +60,31 @@ while True:
                                         product["cantidad"] = quantity_product
                                     break
                                 except ValueError:
-                                    print("\033[91m¡ERROR!. Debe ingresar un número.\033[0m")
+                                    print("¡ERROR!. Debe ingresar un número.")
                             inventory.append(product)
                     break
                 except ValueError:
-                    print("\033[91m¡ERROR!. Debe ingresar un número entero.\033[0m")
+                    print("¡ERROR!. Debe ingresar un número entero.")
 
 #Sección para mostrar el inventario con todos sus elementos
         elif user_election == 2:
             if len(inventory) == 0:
-                print("\033[93m¡El inventario está vacío!\033[0m")
+                print("¡El inventario está vacío!")
             else:
                 for product in inventory:
-                    print(f"\033[92mNombre: {product["nombre"]} | Precio: {product["precio"]} | Cantidad: {product["cantidad"]}\033[0m")
+                    print(f"Nombre: {product["nombre"]} | Precio: {product["precio"]} | Cantidad: {product["cantidad"]}")
 
 #Sección para mostrar las estadísticas totales
         elif user_election == 3:
             total_prices = sum(all_prices)
             total_quantity = sum(all_quantity)
-            print(f"\033[92mValor total del inventario: {total_prices}\033[0m")
-            print(f"\033[92mCantidad total de productos ingresados: {total_quantity}\033[0m")
+            print(f"[92mValor total del inventario: {total_prices}")
+            print(f"Cantidad total de productos ingresados: {total_quantity}")
 
 #Sección para salir de ciclo y cerrar el programa
         else:
-            print("\033[94mUsted eligió salir, ¡CHAO!\033[0m")
+            print("Usted eligió salir, ¡CHAO!")
             break
     except ValueError:
         print("")
-        print("\033[91m¡ERROR!. Debe ingresar un número entero.\033[0m")
+        print("¡ERROR!. Debe ingresar un número entero.")
